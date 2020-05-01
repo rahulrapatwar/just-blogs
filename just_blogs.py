8aec7741185a91c2a3387ba7b59227e1bbad765b
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2020-05-01 15:06:58 -0700

    added dummy posts

--- just_blogs.py
+++ just_blogs.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'ABC XYZ',
+        'title': 'Bolg Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 01, 2020',
+    },
+    {
+        'author': 'New Author',
+        'title': 'Bolg Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 01, 2020',
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
