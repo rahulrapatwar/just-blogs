04b24d3418e8960f41ab5358ba68ba5d05d7bd6e
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2020-05-01 14:46:02 -0700

    added home and about routes

--- just_blogs.py
+++ just_blogs.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'This is the home page'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
